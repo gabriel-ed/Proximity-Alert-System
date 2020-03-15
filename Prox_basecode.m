f44a885fdfc82eeeae69d550885bec72b18e8bb3
Gabriel Smith <gmansmith2020@gmail.com>
2020-03-14 23:22:02 -0400

    added output LEDs (red, yellow, green)
    Visual LED's that show when an object is getting close (red), or further away (yellow to green)

--- Prox_basecode.m
+++ Prox_basecode.m
@@ -7,22 +7,54 @@ clear
 close all
 
 %% Arduino Ultrasonic Distance 
-N = 2000;     %number of data-points to take (iterations)
+%Constant specifications
+port = 'COM3'; %ensure correct com port
+board = 'Uno';
+trigPin = 'D10';
+echoPin = 'D9';
+redPin = 'D7';
+yellowPin = 'D12';
+greenPin = 'D8';
+pinMode = 'DigitalOutput';
+
+
+N = 1000;     %number of data-points to take (iterations)
+
 x = 1:N;
 y = zeros(1, N);
 h = animatedline;
-trigPin = 'D10';
-echoPin = 'D9';
+a = arduino(port, board);
 
-a = arduino('COM3', );
+configurePin(a, redPin, pinMode);
+configurePin(a, yellowPin, pinMode);
+configurePin(a, greenPin, pinMode);
+
 ultrasonicObj = ultrasonic(a, trigPin, echoPin);
 
 for i = 1:N
     y(i) = readDistance(ultrasonicObj);
     xlabel('Iteration');
-    ylabel('Distance');
+    ylabel('Distance (meters)');
     addpoints(h, x(i), y(i));
     drawnow;
+    if y(i) > 1
+        writeDigitalPin(a, greenPin, 1);
+        writeDigitalPin(a, yellowPin, 0);
+        writeDigitalPin(a, redPin, 0);
+    elseif y(i) > 0.5 && y(i) < 0.9
+        writeDigitalPin(a, greenPin, 0);
+        writeDigitalPin(a, yellowPin, 1);
+        writeDigitalPin(a, redPin, 0);
+    else
+        writeDigitalPin(a, greenPin, 0);
+        writeDigitalPin(a, yellowPin, 0);
+        writeDigitalPin(a, redPin, 1);
+    end 
+        
 end
+
+writeDigitalPin(a, greenPin, 0);
+writeDigitalPin(a, yellowPin, 0);
+writeDigitalPin(a, redPin, 0);
 
 
